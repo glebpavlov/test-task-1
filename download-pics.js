c5b7c45f91b2c23a9ed3bdb0f912aa38b14c1fe0
Gleb Pavlov <winox@bk.ru>
2021-02-14 23:54:54 +0300

    Скачал удаленные файлы

---
+++ download-pics.js
@@ -0,0 +1,38 @@
+/**
+ * Скрипт для скачивания файлов и замены ссылок в файле каналов
+ */
+
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+
+const sourceDataPath = './src/assets/stub-data/channels.json';
+
+// получаем список каналов
+const channelsData = JSON.parse(fs.readFileSync(sourceDataPath).toString());
+
+
+channelsData.channelDetails.forEach((channelData) => {
+  Object.keys(channelData.picture).forEach((pictureType) => {
+    channelData.picture[pictureType].forEach((pictureUrl, index) => {
+      if (!pictureUrl) {
+        return;
+      }
+      const filename = path.parse(pictureUrl).base;
+      const filePath = `./src/assets/images/tv/${filename}`;
+      channelData.picture[pictureType][index] = `/assets/images/tv/${filename}`;
+      const file = fs.createWriteStream(filePath);
+
+      https.get(pictureUrl, function (response) {
+        response.pipe(file);
+        file.on('finish', function () {
+          file.close();  // close() is async, call cb after close completes.
+        });
+      }).on('error', function (err) { // Handle errors
+        fs.unlink(filePath, ()=>{}); // Delete the file async. (But we don't check the result)
+      });
+    });
+  })
+})
+fs.writeFileSync(sourceDataPath, JSON.stringify(channelsData, null, 2));
+console.log(sourceDataPath)
